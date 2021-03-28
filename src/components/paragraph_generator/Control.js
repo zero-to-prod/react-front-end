d5a09c1586bdfd8c78686befe02986b69fdf767f
D.A.Smith <dsmith@realclearpolitics.com>
2021-03-27 20:35:02 -0400

    Refactor: adjust styles for send button.

--- src/components/paragraph_generator/Control.js
+++ src/components/paragraph_generator/Control.js
@@ -5,7 +5,7 @@ const Control = ({ title, clicked }) => {
     <Fragment>
       <button
         onClick={clicked}
-        className="btn col-span-2 hover:scale-105 transform transition text-gray-800 font-medium md:text-lg font-bold bg-gray-200 hover:bg-gray-300 py-4 rounded-lg border border-transparent leading-4 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        className="btn col-span-2 hover:scale-105 transform transition text-gray-800 font-medium md:text-lg font-bold bg-gray-200 hover:bg-gray-300 py-3 rounded-lg border border-transparent leading-4 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
       >
         {title}
       </button>
--- src/components/paragraph_generator/Send.js
+++ src/components/paragraph_generator/Send.js
@@ -12,7 +12,7 @@ const Send = ({ number_of_paragraphs }) => {
       <button
         onClick={onClick}
         type="submit"
-        className="w-full h-14 hover:scale-105 transform transition justify-center inline-flex items-center px-3 py-4 border border-transparent text md:text-2xl leading-4 rounded-md shadow-sm text-white font-bold bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        className="w-full h-12 sm:h-14 hover:scale-105 transform transition justify-center inline-flex items-center px-3 py-4 border border-transparent text md:text-2xl leading-4 rounded-md shadow-sm text-white font-bold bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
       >
         {loading ? (
           <div className="loader ease-linear rounded-full border-2 border-t-2 border-gray-200 h-6 w-6" />
